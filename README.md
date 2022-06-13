cee07392232603b71370f73086354df79604fc90
Alexa Acosta <alac@odoo.com>
2022-06-13 18:28:51 +0000

    real estate advertisement module base

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wk2-tech-training
+
+Practice Module following the tutorial: https://www.odoo.com/documentation/15.0/developer/howtos/rdtraining/06_firstui.html#howto-rdtraining-06-firstui
---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real Estate Advertisement",
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv'],
+    'author': "Alexa Acosta",
+    'category': 'Marketing',
+    'description': """
+        Lists all Real Estate advertisements and associated information.
+    """,
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
