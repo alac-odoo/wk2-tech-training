c277a2063781d30b9362a44730247f104d01ee31
Alexa Acosta <alac@odoo.com>
2022-06-14 10:12:51 -0400

    [ADD] estate menus xml file for menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
+
 {
     'name': "Real Estate Advertisement",
     'version': '1.0',
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-        'data/estate_property_views.xml'
+        'data/estate_property_views.xml',
+        'data/estate_menus.xml'
         ],
     'author': "Alexa Acosta",
     'category': 'Marketing',
