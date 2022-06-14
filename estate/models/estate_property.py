91632d8bfad716ad60185ed3b780dd00f7dababc
Alexa Acosta <alac@odoo.com>
2022-06-14 10:58:31 -0400

    [IMP] added default values to model definition

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,21 +7,31 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Contains information about an estate property."
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="Unknown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=lambda self: fields.Date.today(),
+                                    copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('east', 'East'),
                    ('south', 'South'), ('west', 'West')],
         help="If in between directions, choose North or South."
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=[('new', 'New'), ('received', 'Offer Received'),
+                   ('accepted', 'Offer Accepted'), ('sold', 'Sold'),
+                   ('cancelled', 'Cancelled')],
+        help="The status of this property.",
+        default='new',
     )
