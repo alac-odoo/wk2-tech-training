2473d6c5ffe45c0875683b1dac0ed70d68096f44
Alexa Acosta <alac@odoo.com>
2022-06-14 08:44:15 -0400

    [IMP] security access written

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Real Estate Advertisement",
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv'],
+    'author': "Alexa Acosta",
+    'category': 'Marketing',
+    'license': 'OPL-1',
+    'description': """
+        Lists all Real Estate advertisements and associated information.
+    """,
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
